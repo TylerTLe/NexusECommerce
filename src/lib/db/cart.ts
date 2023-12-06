fe30e2959e9a56c8b9fcc9028680d9c27e73f5e0
TylerTLe <tyler.le@edu.sait.ca>
2023-12-05 22:31:14 -0700

    Fix import statements and update data manipulation in cart actions

--- src/lib/db/cart.ts
+++ src/lib/db/cart.ts
@@ -1,5 +1,5 @@
 import { Cart, CartItem, Prisma } from '@prisma/client';
-import prisma from './prisma';
+import { prisma } from './prisma';
 import { cookies } from "next/dist/client/components/headers"
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
@@ -102,12 +102,19 @@ export async function mergeCart(userId: string) {
                     where: {cartId: userCart.id},
                 })
 
-                await tx.cartItem.createMany({ 
-                    data: mergedCartItems.map((item) => ({
-                        cartId: userCart.id,
-                        productId: item.productId,
-                        quantity: item.quantity,
-                    })),
+                await tx.cart.update({
+                    where: {id: userCart.id},
+                    data: {
+                        items: {
+                            createMany: {
+                                data: mergedCartItems.map((item) => ({
+                                    cartId: userCart.id,
+                                    productId: item.productId,
+                                    quantity: item.quantity,
+                                })),
+                            },
+                        },
+                    },
                 })
             } else {
                 await tx.cart.create({
@@ -119,10 +126,10 @@ export async function mergeCart(userId: string) {
                                     productId: item.productId,
                                     quantity: item.quantity,
                                 })),
-                            }
-                        }
-                    }
-                })
+                            },
+                        },
+                    },
+                });
             }
             await tx.cart.delete({
                 where: {id: localCart.id},
